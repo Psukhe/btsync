3062324b29d71d9575ba921da45bfd3bbd3bec22
adunn <alexanderldunn@gmail.com>
2014-12-03 15:43:36 -0800

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'w.hattingh@nitecon.com'
 license          'Apache 2.0'
 description      'Installs/Configures btsync'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 recipe            'btsync', 'Main Btsync configuration'
 depends 'partial_search'
 
